9e73fb2685f854b8ab2711d1fca3d53e5695e14f
aparnats7 <aparna752004@gmail.com>
2025-03-20 10:31:40 +0530

    dependencies updated

--- vite.config.js
+++ vite.config.js
@@ -12,11 +12,11 @@ export default defineConfig({
     },
   },
   server: {
-    port: 5173,
-    strictPort: true,
+    port: 3000,
+    strictPort: false,
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'http://localhost:3001',
         changeOrigin: true,
       },
     },
