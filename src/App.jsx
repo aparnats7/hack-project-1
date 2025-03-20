178e18c5c43e6433021a8058f674532d56074c60
aparnats7 <aparna752004@gmail.com>
2025-03-20 10:29:39 +0530

    app.jsx updated

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,11 @@ import NotFound from "./pages/NotFound";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import ForgotPassword from "./pages/ForgotPassword";
+import ContactSales from "./pages/ContactSales";
+import ScheduleDemo from "./pages/ScheduleDemo";
+import Pricing from "./pages/Pricing";
+import LearnMore from './pages/LearnMore';
+import TermsAndConditions from './pages/TermsAndConditions';
 
 const queryClient = new QueryClient();
 
@@ -21,6 +26,11 @@ const App = () => (
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/contact-sales" element={<ContactSales />} />
+        <Route path="/schedule-demo" element={<ScheduleDemo />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/learn-more" element={<LearnMore />} />
+        <Route path="/terms-and-conditions" element={<TermsAndConditions />} />
         {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
         <Route path="*" element={<NotFound />} />
       </Routes>
