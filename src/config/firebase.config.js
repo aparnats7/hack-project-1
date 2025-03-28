1578dc57f33b68a158eb900bf8516dc4444d554d
aparnats7 <aparna752004@gmail.com>
2025-03-28 18:20:23 +0530

    firebase added

---
+++ src/config/firebase.config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCmj_YRpO_5QO_ST-q84PmrArihZt-1I84",
+  authDomain: "veritrustai.firebaseapp.com",
+  projectId: "veritrustai",
+  storageBucket: "veritrustai.appspot.com",
+  messagingSenderId: "1072239221222",
+  appId: "1:1072239221222:web:9992999299299299299299"
+};
