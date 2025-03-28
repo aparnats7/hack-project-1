18b53abde39cede8bc02edf0d5dde5b9f792f4a5
aparnats7 <aparna752004@gmail.com>
2025-03-28 18:21:39 +0530

    use toast modified

--- src/hooks/use-toast.ts
+++ src/hooks/use-toast.ts
@@ -1,3 +1,4 @@
+// Inspired by react-hot-toast library
 import * as React from "react"
 
 import type {
@@ -25,7 +26,7 @@ const actionTypes = {
 let count = 0
 
 function genId() {
-  count = (count + 1) % Number.MAX_SAFE_INTEGER
+  count = (count + 1) % Number.MAX_VALUE
   return count.toString()
 }
 
@@ -38,7 +39,7 @@ type Action =
     }
   | {
       type: ActionType["UPDATE_TOAST"]
-      toast: Partial<ToasterToast>
+      toast: Partial<ToasterToast> & Pick<ToasterToast, "id">
     }
   | {
       type: ActionType["DISMISS_TOAST"]
